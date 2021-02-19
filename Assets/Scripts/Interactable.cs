346dcb8413f4fed34e96c17bd6e1e0fb95fcbaa0
sim2kid <sim2kid@users.noreply.github.com>
2021-02-19 03:21:13 -0600

    Adds a few script files for basic engine stuff
    Includes Click & Drag and Interactables

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public SpriteMouseState State 
+    {
+        get;
+        private set;
+    }
+    
+    
+    void Start()
+    {
+        //Initialize Variables
+        State = SpriteMouseState.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum SpriteMouseState {
+    None, // Default State
+    Hover, // While mouse is hovering (unclicked)
+    Click, // When the mouse clicks down
+    Hold, // When mouse is hovering (clicked)
+    Drag, // When mouse is hovering, but held clicked first
+    Release // when mouse clicks up
+}
