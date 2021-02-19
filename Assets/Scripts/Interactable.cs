e9b6ccde0d41b65920852793bc72b0b88ee10168
sim2kid <sim2kid@users.noreply.github.com>
2021-02-19 03:55:17 -0600

    Adds a Mouse manager script

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour
+public class Interactable : MonoBehaviour, IInteractable
 {
     public SpriteMouseState State 
     {
