54b9cf25aef78831c5484dd3806b1ff04c664716
sim2kid <sim2kid@users.noreply.github.com>
2021-02-24 12:52:11 -0600

    Make MouseManager see raw inputs using the input asset

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -3,22 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
+[DisallowMultipleComponent]
 public class MouseManager : MonoBehaviour
 {
-    
-    void Start()
+    private PlayerInput m_PlayerInput;
+    private InputAction m_LocationAction;
+    private InputAction m_HoldAction;
+
+    private ClickType click;
+
+    private void Start()
     {
-        //Initialization
+        click = ClickType.Clear;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
-    }
+        if (m_PlayerInput == null) 
+        {
+            m_PlayerInput = GetComponent<PlayerInput>();
+            m_LocationAction = m_PlayerInput.actions["Location"];
+            m_HoldAction = m_PlayerInput.actions["Hold"];
+        }
 
-    public void InputPosition(InputAction.CallbackContext context) 
-    {
-    
+        Vector2 screenLocation = m_LocationAction.ReadValue<Vector2>();
+        var hold = m_HoldAction.ReadValue<float>();
+
+        if (click == ClickType.Unclick)
+        {
+            click = ClickType.Clear;
+        }
+        if (click == ClickType.Click)
+        {
+            click = ClickType.Hold;
+        }
+
+        if (click == ClickType.Clear && hold > 0) 
+        {
+            click = ClickType.Click;
+        }
+        if (click == ClickType.Hold && hold == 0)
+        {
+            click = ClickType.Unclick;
+        }
+
+        //Debug.Log("Click: " + click + ", Hold: " + hold + ", Location: (" + screenLocation.x + ", " + screenLocation.y + ")");
     }
+}
+
+enum ClickType 
+{
+    Click,
+    Hold,
+    Unclick,
+    Clear
 }
