b871b577ae2168e9333e8c9a5f138672fe6c4016
sim2kid <sim2kid@users.noreply.github.com>
2021-02-24 14:29:09 -0600

    Finish basic interactable scripts

--- Assets/Scripts/IInteractable.cs
+++ Assets/Scripts/IInteractable.cs
@@ -7,4 +7,6 @@ public interface IInteractable
     void UpdateMouseState(ClickType clickType);
     void OnLeave(ClickType clickType);
     void OnEnter(ClickType clickType);
+    void OnClick();
+    void OnUnclick();
 }
--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour, IInteractable
 {
-    public MouseState State 
-    {
-        get;
-        private set;
-    }
-    
-    
     void Start()
     {
         //Initialize Variables
-        State = MouseState.None;
     }
 
     // Update is called once per frame
@@ -22,31 +14,33 @@ public class Interactable : MonoBehaviour, IInteractable
     {
         
     }
+
     public void UpdateMouseState(ClickType clickType) 
     {
         if (clickType == ClickType.Click) {
-            Debug.Log("I've been clicked!");
+            OnClick();
         }
         if (clickType == ClickType.Unclick)
         {
-            Debug.Log("Oh wait.. nvm...");
+            OnUnclick();
         }
     }
-    public void OnEnter(ClickType clickType)
+
+    //Event Responses
+    public virtual void OnClick() 
+    {
+        Debug.Log("I've been clicked!");
+    }
+    public virtual void OnUnclick() 
+    {
+        Debug.Log("Oh wait.. nvm...");
+    }
+    public virtual void OnEnter(ClickType clickType)
     {
         Debug.Log("A mouse entered while in state: " + clickType);
     }
-    public void OnLeave(ClickType clickType) 
+    public virtual void OnLeave(ClickType clickType) 
     {
         Debug.Log("A mouse left while in state: " + clickType);
     }
-}
-
-public enum MouseState {
-    None, // Default State
-    Hover, // While mouse is hovering (unclicked)
-    Click, // When the mouse clicks down
-    Hold, // When mouse is hovering (clicked)
-    Drag, // When mouse is hovering, but held clicked first
-    Release // when mouse clicks up
 }
