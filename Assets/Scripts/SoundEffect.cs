4ab52e0626d8a034550af2cc25ab25719fd6e3d2
sim2kid <sim2kid@users.noreply.github.com>
2021-02-25 23:46:50 -0600

    Adds a quick patch for the sound effect into the input system

---
+++ Assets/Scripts/SoundEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffect : CycleSprite
+{
+    [SerializeField]
+    private AudioClip soundToMake;
+
+    protected override void OnCycle()
+    {
+        // Put in the call to the sound files here
+        base.OnCycle();
+    }
+}
