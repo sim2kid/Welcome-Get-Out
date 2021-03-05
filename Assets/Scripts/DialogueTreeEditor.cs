123a897ded47326eb0b8f37ce67468f9ad6e6c23
sim2kid <sim2kid@users.noreply.github.com>
2021-03-05 10:23:07 -0600

    Fixed the script so it can compile

--- Assets/Scripts/DialogueTreeEditor.cs
+++ Assets/Scripts/DialogueTreeEditor.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿#if UNITY_EDITOR
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
@@ -91,3 +90,4 @@ public class DialogueTreeEditor : Editor
         EditorGUI.LabelField(rect, "Dialogue");
     }
 }
+#endif
