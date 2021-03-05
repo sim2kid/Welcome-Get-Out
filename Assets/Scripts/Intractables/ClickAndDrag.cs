16c3b644c7522f06682a20427b8c0b69122c517d
sim2kid <sim2kid@users.noreply.github.com>
2021-03-05 05:43:04 -0600

    Folder Spelling Error Fixed

--- Assets/Scripts/Intractables/ClickAndDrag.cs
+++ Assets/Scripts/Intractables/ClickAndDrag.cs
@@ -7,7 +7,7 @@ public class ClickAndDrag : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("TODO");
+
     }
 
     // Update is called once per frame
