6546641ab583ce5f39903fbfb885c9af268559a2
sim2kid <sim2kid@users.noreply.github.com>
2021-03-19 02:06:25 -0500

    Basic button works
    Also added composer and all the audio files

---
+++ Assets/Scripts/Level-3/Level3Composser.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Level3Composser : MonoBehaviour
+{
+    [SerializeField] 
+    private int epoch;
+    [SerializeField]
+    private ProgressBar progressBar;
+    [SerializeField]
+    NarratorController narrator;
+
+    private UnityEvent epochTrigger;
+
+    void Start()
+    {
+        epoch = 0;
+        progressBar.modifyProgress(-1);
+        epochTrigger = new UnityEvent();
+        epochTrigger.AddListener(TriggerEpoch);
+    }
+
+    public void TriggerEpoch() 
+    {
+        epoch++;
+    }
+
+    public void ButtonPress() 
+    {
+        switch (epoch)
+        {
+            case 0:
+                progressBar.modifyProgress(0.02f);
+                narrator.Trigger(epochTrigger);
+                break;
+            case 1:
+                progressBar.modifyProgress(0.02f);
+                narrator.Trigger(epochTrigger);
+                break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        switch (epoch)
+        {
+            case 0:
+                if (progressBar.GetProgress() > 0.90)
+                {
+                    progressBar.modifyProgress(-0.5f * Time.fixedDeltaTime);
+                }
+                if (progressBar.GetProgress() > 0.15) 
+                {
+                    progressBar.modifyProgress(-0.2f * Time.fixedDeltaTime);
+                }
+                if (progressBar.GetProgress() > 0.09)
+                {
+                    progressBar.modifyProgress(-0.2f * Time.fixedDeltaTime);
+                }
+
+                progressBar.modifyProgress(-0.05f * Time.fixedDeltaTime);
+                break;
+            case 1:
+                if (progressBar.GetProgress() > 0.90)
+                {
+                    progressBar.modifyProgress(-0.5f * Time.fixedDeltaTime);
+                }
+                if (progressBar.GetProgress() > 0.15)
+                {
+                    progressBar.modifyProgress(-0.2f * Time.fixedDeltaTime);
+                }
+                break;
+        }
+        
+    }
+}
