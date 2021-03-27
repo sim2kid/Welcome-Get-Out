f93b9daeb7c25ec20329be90c734a7c75cff2782
sim2kid <sim2kid@users.noreply.github.com>
2021-03-27 09:54:02 -0500

    Added skip to level 4
    Will be replaced in the level-4 branch

---
+++ Assets/Scripts/Level-4/level4Composer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level4Composer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level-5");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
