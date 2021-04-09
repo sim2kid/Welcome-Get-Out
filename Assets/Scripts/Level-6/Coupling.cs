e865aa53d7b50d0c416988e6e6d1b74b29ce9c95
sim2kid <sim2kid@users.noreply.github.com>
2021-04-08 21:15:16 -0500

    Added sprites to scene and added some scripts
    Including Gear Spinners and Coupling scripts

---
+++ Assets/Scripts/Level-6/Coupling.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Coupling : MonoBehaviour
+{
+    [SerializeField]
+    GameObject toBindto;
+    [SerializeField]
+    float radius;
+    [SerializeField]
+    float uncoupleOffset;
+    [SerializeField]
+    UnityEvent onCouple;
+    [SerializeField]
+    UnityEvent onUnCouple;
+
+    private void Start()
+    {
+        TryToCouple();
+    }
+
+    public void TryToCouple() 
+    {
+        if (((Vector2)toBindto.transform.position
+            - (Vector2)transform.position).magnitude <= radius) 
+        {
+            Couple();
+        }
+    }
+
+    public void Couple() 
+    {
+        toBindto.transform.parent = this.transform;
+        toBindto.transform.localPosition = Vector3.zero;
+        toBindto.transform.localRotation = new Quaternion();
+        onCouple.Invoke();
+    }
+    public void UnCouple()
+    {
+        toBindto.transform.parent = this.transform.parent;
+        toBindto.transform.localPosition = new Vector3(toBindto.transform.localPosition.x,
+            toBindto.transform.localPosition.y, uncoupleOffset);
+        onUnCouple.Invoke();
+    }
+}
---
+++ Assets/Scripts/Level-6/GearSpinner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearSpinner : MonoBehaviour
+{
+    [SerializeField]
+    private bool goClockwise;
+    [SerializeField]
+    private float rpm;
+    [SerializeField]
+    public bool shake;
+
+    void Update()
+    {
+        if (shake)
+            goClockwise = !goClockwise;
+        this.transform.Rotate(new Vector3(0, 0, rpm * 6 * Time.deltaTime * (goClockwise ? -1 : 1)));
+    }
+
+    public void Shake(bool torf) 
+    {
+        shake = torf;
+    }
+}
