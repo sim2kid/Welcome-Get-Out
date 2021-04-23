a387e7906f8d3b0f15d50a7a9dfd234a6824d150
sim2kid <sim2kid@users.noreply.github.com>
2021-04-23 01:17:36 -0500

    Code Checker added
    it checks completed code blocks

---
+++ Assets/Scripts/Level-7/CodeChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeChecker : MonoBehaviour
+{
+    [SerializeField]
+    List<Coupling> codeBlocks;
+
+    public int codeFixedAmount() 
+    {
+        int fixedBlocks = 0;
+        foreach (Coupling c in codeBlocks) 
+        {
+            if (c.isCoupled)
+                fixedBlocks++;
+        }
+        return fixedBlocks++;
+    }
+}
---
+++ Assets/Scripts/Level-7/level7Composer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level7Composer : MonoBehaviour
+{
+    [SerializeField]
+    NarratorController narrator;
+
+    void Start()
+    {
+        
+    }
+
+
+}
