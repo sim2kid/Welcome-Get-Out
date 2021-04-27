2a50c046af887b037152c5abe704a8c144aa5219
sim2kid <sim2kid@users.noreply.github.com>
2021-04-26 20:43:30 -0500

    Set up most scripts and persistent data

---
+++ Assets/Scripts/Credits/creditsComposer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Persistent))]
+public class creditsComposer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject backButton;
+    [SerializeField]
+    NarratorController narrator;
+    [SerializeField]
+    DialogueTree MeanCredits;
+    [SerializeField]
+    DialogueTree NiceCredits;
+
+    Persistent data;
+    void Start()
+    {
+        data = GetComponent<Persistent>();
+        data.UpdateScene(SceneManager.GetActiveScene().name);
+        if (data.LastScene == "Level-1" || data.LastScene == "Level-0")
+            backButton.SetActive(true);
+        else
+            backButton.SetActive(false);
+        if (data.NarratorIsNice)
+        {
+            narrator.NewNarration(NiceCredits, 0);
+        }
+        else 
+        {
+            narrator.NewNarration(MeanCredits, 0);
+        }
+    }
+
+    public void BackButton() 
+    {
+        SceneManager.LoadScene(data.LastScene);
+    }
+}
---
+++ Assets/Scripts/Persistent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persistent : MonoBehaviour
+{
+    private static Persistent s_Instance = null;
+    public bool NarratorIsNice;
+    public string LastScene 
+    {
+        get;
+        private set;
+    }
+    public string CurrentScene
+    {
+        get;
+        private set;
+    }
+    public bool GameWasClosed;
+
+    void Awake()
+    {
+        if (s_Instance == null)
+        {
+            s_Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            Initilize();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Initilize() 
+    {
+        NarratorIsNice = false;
+        CurrentScene = "";
+        LastScene = "";
+        // Actually need to check for this!!
+        GameWasClosed = false;
+    }
+
+    public void UpdateScene(string sceneName) 
+    {
+        if (CurrentScene != sceneName)
+        {
+            LastScene = CurrentScene;
+            CurrentScene = sceneName;
+        }
+    }
+}
