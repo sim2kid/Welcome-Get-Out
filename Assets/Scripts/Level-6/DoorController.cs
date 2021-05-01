e5d8a71a97603b63f2410823f5a6ab624e702df6
sim2kid <sim2kid@users.noreply.github.com>
2021-05-01 14:56:05 -0500

    Level-6 done

--- Assets/Scripts/Level-6/DoorController.cs
+++ Assets/Scripts/Level-6/DoorController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DoorController : MonoBehaviour
 {
@@ -9,11 +10,17 @@ public class DoorController : MonoBehaviour
     [SerializeField]
     Vector2 desiredLocation;
 
+    [SerializeField]
+    public UnityEvent OnMarch, OnHault;
+
     public bool isMarching;
+    bool lastMarch;
+    
 
     void Start()
     {
         isMarching = false;
+        lastMarch = false;
     }
 
     // Update is called once per frame
@@ -22,6 +29,22 @@ public class DoorController : MonoBehaviour
         if (isMarching) 
         {
             transform.position += (Vector3)(desiredLocation - (Vector2)transform.position).normalized * marchRate * Time.deltaTime;
+            if (Mathf.Sqrt((desiredLocation - (Vector2)transform.position).SqrMagnitude()) < 0.1f) 
+            {
+                isMarching = false;
+            }
+        }
+        if (lastMarch != isMarching) 
+        {
+            if (isMarching)
+            {
+                OnMarch.Invoke();
+            }
+            else 
+            {
+                OnHault.Invoke();
+            }
+            lastMarch = isMarching;
         }
     }
 }
